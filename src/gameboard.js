934bdd2264f0242e9420efde6d42c193dbfa13aa
namlev <mikipatkowski@protonmail.com>
2024-08-13 11:49:25 +0200

    Add placing to gameboard

---
+++ src/gameboard.js
@@ -0,0 +1,60 @@
+export default class Gameboard {
+  constructor() {
+    this.xMax = 7
+    this.yMax = 7
+    this.board = new Array(8)
+    for (let i = 0; i <= this.xMax; i++) {
+      this.board[i] = new Array(8)
+    }
+  }
+
+  #isOccupied(x, y) {
+    return this.board[x][y]
+  }
+
+  #isOutOfBounds(x, y) {
+    return x > this.xMax || y > this.yMax
+  }
+
+  #throwIfInvalidVertically(ship, x, y) {
+    for (let i = 0; i < ship.length; i++) {
+      if (this.#isOccupied(x, y + i)) {
+        throw new Error('Ships overlap')
+      }
+      if (this.#isOutOfBounds(x, y + i)) {
+        throw new Error('Ship placed out of board bounds')
+      }
+    }
+  }
+
+  #throwIfInvalidHorizontally(ship, x, y) {
+    for (let i = 0; i < ship.length; i++) {
+      if (this.#isOutOfBounds(x + i, y)) {
+        throw new Error('Ship placed out of board bounds')
+      }
+      if (this.#isOccupied(x + i, y)) {
+        throw new Error('Ships overlap')
+      }
+    }
+  }
+
+  #placeVertically(x, y, ship) {
+    this.#throwIfInvalidVertically(ship, x, y)
+    for (let i = 0; i < ship.length; i++) {
+      this.board[x][y + i] = ship
+    }
+  }
+
+  #placeHorizontally(x, y, ship) {
+    this.#throwIfInvalidHorizontally(ship, x, y)
+    for (let i = 0; i < ship.length; i++) {
+      this.board[x + i][y] = ship
+    }
+  }
+
+  place(coords, ship, verticle = false) {
+    const [x, y] = coords
+    if (verticle) return this.#placeVertically(x, y, ship)
+    return this.#placeHorizontally(x, y, ship)
+  }
+}
---
+++ test/gameboard.test.js
@@ -0,0 +1,83 @@
+import Gameboard from '../src/gameboard'
+import Ship from '../src/ship'
+
+let gameBoard
+let ship
+
+beforeEach(() => {
+  gameBoard = new Gameboard()
+  ship = new Ship(2)
+})
+
+test('Place a ship horizontally', () => {
+  gameBoard.place([0, 0], ship)
+  expect(gameBoard.board[1][0]).toBe(ship)
+})
+
+test('Place a ship vertically', () => {
+  gameBoard.place([0, 0], ship, true)
+  expect(gameBoard.board[0][1]).toBe(ship)
+})
+
+test('Place out of bonds', () => {
+  expect(() => gameBoard.place([7, 0], ship)).toThrow(
+    'Ship placed out of board bounds'
+  )
+})
+
+test('Place 2 overlapping ships', () => {
+  gameBoard.place([0, 0], ship)
+  const ship2 = new Ship(2)
+  expect(() => {
+    gameBoard.place([1, 0], ship2)
+  }).toThrow('Ships overlap')
+})
+
+test('Hit a ship horizontally', () => {
+  ship.hit = jest.fn()
+  gameBoard.place([0, 0], ship)
+  gameBoard.receiveAttack([1, 0])
+  expect(ship.hit).toHaveBeenCalled()
+})
+
+test('Hit a ship vertically', () => {
+  ship.hit = jest.fn()
+  gameBoard.place([0, 0], ship, true)
+  gameBoard.receiveAttack([0, 1])
+  expect(ship.hit).toHaveBeenCalled()
+})
+
+test('Miss a ship', () => {
+  ship.hit = jest.fn()
+  gameBoard.place([0, 0], ship, true)
+  gameBoard.receiveAttack([1, 1])
+  expect(ship.hit).not.toHaveBeenCalled()
+})
+
+test('Shoot empty board', () => {
+  gameBoard.receiveAttack([0, 0])
+  gameBoard.receiveAttack([1, 0])
+  gameBoard.receiveAttack([2, 0])
+  expect(gameBoard.missedCount).toBe(3)
+})
+
+test('Both missed and hit attacks count', () => {
+  gameBoard.place([0, 0], ship)
+  gameBoard.receiveAttack([0, 0])
+  gameBoard.receiveAttack([1, 1])
+  gameBoard.receiveAttack([2, 2])
+  expect(gameBoard.missedCount).toBe(2)
+})
+
+test('All ships are sunk', () => {
+  gameBoard.place([0, 0], ship)
+  gameBoard.receiveAttack([0, 0])
+  gameBoard.receiveAttack([1, 0])
+  expect(gameBoard.isAllShipsSunk()).toBe(true)
+})
+
+test('Not all ships are sunk', () => {
+  gameBoard.place([0, 0], ship)
+  gameBoard.receiveAttack([0, 0])
+  expect(gameBoard.isAllShipsSunk()).toBe(false)
+})
