3c2d0beb20694854d4267994af44049004f20e33
namlev <mikipatkowski@protonmail.com>
2024-08-18 12:28:44 +0200

    Add gameboard

--- src/dom/createHomePage.js
+++ src/dom/createHomePage.js
@@ -29,6 +29,47 @@ const addHeader = () => {
   header.appendChild(github)
 }
 
+function addShipCell(board) {
+  const cell = document.createElement('div')
+  cell.classList.add('cell')
+  board.appendChild(cell)
+}
+
+function addRowCell(i, board) {
+  const rowNum = document.createElement('div')
+  rowNum.textContent = String(i)
+  board.appendChild(rowNum)
+}
+
+function addColumnCell(A, i, board) {
+  const colName = document.createElement('div')
+  colName.textContent = String.fromCharCode(A + i)
+  board.appendChild(colName)
+}
+
+function addCells(board) {
+  const A = 65
+  for (let i = 0; i < 10; i++) {
+    addColumnCell(A, i, board)
+  }
+  for (let i = 1; i <= 10; i++) {
+    addRowCell(i, board)
+    for (let j = 0; j < 10; j++) {
+      addShipCell(board)
+    }
+  }
+}
+
+const createBoard = className => {
+  const board = document.createElement('div')
+  board.classList.add('board')
+  board.classList.add(className)
+  const emptyCell = document.createElement('div')
+  board.appendChild(emptyCell)
+  addCells(board)
+  return board
+}
+
 const addMain = () => {
   const main = document.querySelector('#main')
   const gameGrid = document.createElement('div')
@@ -49,8 +90,13 @@ const addMain = () => {
   enemyBanner.disabled = true
   enemyBanner.textContent = 'OPPONENT'
 
+  const playerBoard = createBoard('player-board')
+  const enemyBoard = createBoard('enemy-board')
+
   playerDiv.appendChild(fleetBanner)
+  playerDiv.appendChild(playerBoard)
   enemyDiv.appendChild(enemyBanner)
+  enemyDiv.appendChild(enemyBoard)
   gameGrid.appendChild(playerDiv)
   gameGrid.appendChild(enemyDiv)
   main.appendChild(gameGrid)
